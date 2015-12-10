4e109ff2ee48ba30a2c775f73be24607f5190384
Jeremy Dulong <dulong_jere@jeremys-macbook-air.local>
2015-12-10 15:56:19 -0500

    Initial Commit

---
+++ Map Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Map Demo
+//
+//  Created by Jeremy Dulong on 12/10/15.
+//  Copyright © 2015 Jeremy Dulong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
